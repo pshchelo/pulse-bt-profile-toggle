de336eb911e02bafdd2d865ff170fd3c40dba43a
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2020-02-06 23:35:38 +0200

    Rewrite using notify2
    should technically become GNOME-independent, using DBus interface for
    desktop notifications more natively.
    
    Added icons for HeadSet and HeadPhones.

--- bttoggle.py
+++ bttoggle.py
@@ -1,54 +1,56 @@
 #!/usr/bin/python3
-import logging
+import notify2
+import pulsectl
 
-import gi
-from gi.repository import GObject
-gi.require_version('Notify', '0.7')
-from gi.repository import Notify # noqa
-import pulsectl # noqa
-
-APP_NAME = "BT profile toggle"
-LOG = logging.getLogger(APP_NAME)
+APP_NAME = "Bluetooth audio profile toggle"
 
 
-class BtProfileToggle(GObject.Object):
-    def __init__(self):
-        super().__init__()
-        # lets initialise with the application name
-        Notify.init(APP_NAME)
-        self.pulse = pulsectl.Pulse(APP_NAME)
+def get_icon_for_profile(profile):
+    if "head" in profile.lower():
+        return "audio-headset-symbolic"
+    elif 'a2dp' in profile.lower():
+        return "audio-headphones-symbolic"
 
-    def _send_notification(self, text, file_path_to_icon=""):
 
-        n = Notify.Notification.new(APP_NAME, text, file_path_to_icon)
-        n.set_timeout(Notify.EXPIRES_DEFAULT)
-        n.show()
+def send_notification(text, icon="dialog-info"):
+    notify2.init(APP_NAME)
+    n = notify2.Notification(text, icon=icon)
+    n.timeout = notify2.EXPIRES_DEFAULT
+    n.hints["transient"] = True
+    n.hints["category"] = "device"
+    n.show()
 
-    def bt_toggle(self):
-        btcards = list(c for c in self.pulse.card_list() if "bluez" in c.name)
-        if not btcards:
-            self._send_notification("No active BT card found")
-            return
-        elif len(btcards) > 1:
-            self._send_notification("Multiple BT cards is not supported yet")
-            return
-        btcard = btcards[0]
-        toggle_to = list(
-            p for p in btcard.profile_list if (
-                p.name not in ('off', btcard.profile_active.name)))
-        if not toggle_to:
-            self._send_notification("No profile to toggle to found")
-            return
-        elif len(toggle_to) > 1:
-            self._send_notification(
-                "Multiple alternative BT profiles not supported")
-            return
-        self.pulse.card_profile_set(btcard, toggle_to[0].name)
-        LOG.info(btcard.profile_active.name)
-        self._send_notification(
-            f"Set BT profile: {btcard.profile_active.name}")
+
+def toggle():
+    pulse = pulsectl.Pulse(APP_NAME)
+    btcards = list(c for c in pulse.card_list() if "bluez" in c.name)
+    if not btcards:
+        send_notification("No active Bluetooth audio found",
+                          icon="dialog-error")
+        return
+    elif len(btcards) > 1:
+        send_notification("Multiple Bluetooth audios is not supported yet")
+        return
+    btcard = btcards[0]
+    toggle_to = list(
+        p for p in btcard.profile_list if (
+            p.name not in ("off", btcard.profile_active.name)))
+    if not toggle_to:
+        send_notification("No alternarive Bluetooth audio profile found",
+                          icon="dialog-error")
+        return
+    elif len(toggle_to) > 1:
+        send_notification(
+            "Multiple alternative Bluetooth profiles not supported"
+        )
+        return
+    pulse.card_profile_set(btcard, toggle_to[0].name)
+    active_profile = btcard.profile_active.name
+    send_notification(
+        f"Using {active_profile.upper()} Bluetooth audio profile",
+        icon=get_icon_for_profile(active_profile)
+    )
 
 
 if __name__ == "__main__":
-    toggler = BtProfileToggle()
-    toggler.bt_toggle()
+    toggle()
